217029a60546a5830e265f6bf1222b0b6e915b32
Gustavo <gustavomendes.medeiros@gmail.com>
2020-07-20 13:02:34 -0300

    Add Uncle Bob's structure (end of 14th chapter)

---
+++ src/ArgsExceptionTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgsExceptionTest {
+    @Test
+    public void testUnexpectedMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.UNEXPECTED_ARGUMENT, 'x', null);
+        assertEquals("Argument -x unexpected.", e.errorMessage());
+    }
+
+    @Test
+    public void testMissingStingMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.MISSING_STRING, 'x', null);
+        assertEquals("Could not find string parameter for -x.", e.errorMessage());
+    }
+
+    @Test
+    public void testInvalidIntegerMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.INVALID_INTEGER, 'x', "Forty Two");
+        assertEquals("Argument -x expects an integer but was 'Forty Two'.", e.errorMessage());
+    }
+
+    @Test
+    public void testMissingIntegerMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.MISSING_INTEGER, 'x', null);
+        assertEquals("Could not find integer parameter for -x.", e.errorMessage());
+    }
+
+    @Test
+    public void testInvalidDoubleMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.INVALID_DOUBLE, 'x', "Forty Two");
+        assertEquals("Argument -x expects a double but was 'Forty Two'.", e.errorMessage());
+    }
+
+    @Test
+    public void testMissingDoubleMessage() throws Exception {
+        ArgsException e = new ArgsException(ArgsException.ErrorCode.MISSING_DOUBLE, 'x', null);
+        assertEquals("Could not find double parameter for -x.", e.errorMessage());
+    }
+}
