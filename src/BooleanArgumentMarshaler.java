0fbb63016b83494979c94ae82fffbd40ae3cdd3c
Gustavo <gustavomendes.medeiros@gmail.com>
2020-07-20 13:09:08 -0300

    Splits Marshaler classes into it's own files

---
+++ src/BooleanArgumentMarshaler.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public class BooleanArgumentMarshaler implements ArgumentMarshaler {
+    private boolean booleanValue = false;
+
+    @Override
+    public void set(Iterator<String> currentArgument) throws ArgsException {
+        booleanValue = true;
+    }
+
+    @Override
+    public Object get() {
+        return booleanValue;
+    }
+}
+
